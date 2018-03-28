329147335516f546dc7e3c45a8f939a2e71b324b
Robert Sanders <rsanders@cybraics.com>
2018-03-27 23:12:22 -0400

    tweaks I don't exactly remember

--- main.go
+++ main.go
@@ -28,11 +28,31 @@ var (
 	debug      = false
 )
 
+var (
+	flushString = "\n__FLUSH__\n"
+)
+
+type LineChan chan *string
+
 // Writer reads strings from a channel and writes them to a RELP server
-func Writer(c chan string, exitChan chan int, client relp.Client) error {
+func Writer(c LineChan, exitChan chan int, client relp.Client) error {
 	defer func() { exitChan <- 1 }()
+	ticker := time.NewTicker(10 * time.Second)
+	var line *string
+	var more bool
 	for {
-		line, more := <-c
+		// read new string and flush the output channel periodically
+		select {
+		case line, more = <-c:
+			if more {
+				client.SendString(*line)
+			}
+			break
+		case <-ticker.C:
+			client.Flush()
+			break
+		}
+
 		if !more {
 			if debug {
 				log.Debug("Writer exiting due to empty channel")
@@ -42,12 +62,11 @@ func Writer(c chan string, exitChan chan int, client relp.Client) error {
 			}
 			return client.Close()
 		}
-		client.SendString(line)
 	}
 }
 
 // StartWriter creates a connection and starts a Writer
-func StartWriter(c chan string, exitChan chan int) error {
+func StartWriter(c LineChan, exitChan chan int) error {
 	conn, err := net.Dial("tcp", *host)
 	if err != nil {
 		log.Fatal("can't connect to server")
@@ -61,7 +80,14 @@ func StartWriter(c chan string, exitChan chan int) error {
 		conn = tls.Client(conn, &tlsconfig)
 	}
 
-	client, err := relp.NewClientFrom(conn, *windowSize)
+	eventChan := make(chan relp.ClientEvent, 20)
+	go func() {
+		for event := range eventChan {
+			log.Print("RECEIVED EVENT: ", event)
+		}
+	}()
+
+	client, err := relp.NewClientFrom(conn, *windowSize, nil)
 	if err != nil {
 		log.Fatal("can't create or connect to server ", err.Error())
 		return err
@@ -69,15 +95,17 @@ func StartWriter(c chan string, exitChan chan int) error {
 	return Writer(c, exitChan, client)
 }
 
-func copyLines(input *bufio.Reader, output chan string) error {
+func copyLines(input *bufio.Reader, output LineChan) error {
 	for {
 		line, err := input.ReadString('\n')
-		if line != "" {
-			if debug {
-				log.Info("Got string: ", line)
-			}
-			output <- strings.TrimRight(line, "\n\r")
+		if debug {
+			log.Info("Got string: ", line)
 		}
+		trimmed := strings.TrimRight(line, "\n\r")
+		if trimmed != "" {
+			output <- &trimmed
+		}
+		// terminate on EOF
 		if err != nil {
 			return err
 		}
@@ -90,7 +118,7 @@ func main() {
 
 	log.Info("Starting up!")
 
-	pipeline := make(chan string, *bufSize)
+	pipeline := make(LineChan, *bufSize)
 	exitChan := make(chan int)
 
 	for i := 0; i < *writers; i++ {
